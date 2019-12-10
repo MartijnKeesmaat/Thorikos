b03cbeb8bce4af6e8a085d999f1879c24273469a
Martijn <mpkeesmaat@gmail.com>
2019-12-10 20:50:25 +0100

    add jsDoc comments

--- map-data.js
+++ map-data.js
@@ -1,27 +1,24 @@
+// Load data file
+// TODO make this dynamic with an upload button
 fetch('data.json')
   .then(response => response.json())
   .then(json => handleData(json));
 
 const handleData = data => {
-  // console.log(data);
-
-  /*
-    contextNumber deconstructed
-    "T13-124-1-A"
-    13 = year
-    124 = macro
-    1 = meso
-    A = micro
-  */
+  const contextNumber = getContextNumberDetails(data[0].CONTEXT);
+  console.log(contextNumber);
 
   const map = {};
-  const countedContextCodes = countContextCodes(data, map);
-
-  const contextNumber = getContextNumberDetails(data[0].CONTEXT);
+  const countedContextCodes = countMacroCodes(data, map);
   console.log(countedContextCodes);
 };
 
-function countContextCodes(data, map) {
+/**
+ * @param {arr} data (needs to contain an CONTEXT key)
+ * @param {obj} map (empty)
+ * @returns {obj} a map of the occurances of each macro code
+ */
+function countMacroCodes(data, map) {
   data.forEach(finding => {
     const macro = getContextNumberDetails(finding.CONTEXT).macro;
     if (!map[macro]) {
@@ -30,6 +27,16 @@ function countContextCodes(data, map) {
   });
   return map;
 }
+
+/**
+ * @param {str} contextNumber ("T13-124-1-A")
+ * @returns {obj} of the individual values of the context code (see below)
+    "T13-124-1-A"
+    13 = year
+    124 = macro
+    1 = meso
+    A = micro
+ */
 
 function getContextNumberDetails(contextNumber) {
   if (!contextNumber) return '';
